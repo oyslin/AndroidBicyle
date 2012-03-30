719329cf374203891e17ace8535ee9a7429e1ab1
Walt Ouyang <oyslin@163.com>
2012-03-30 17:10:21 +0800

    first commit
    Signed-off-by: Walt Ouyang <oyslin@163.com>

---
+++ src/com/walt/BicyleActivity.java
@@ -0,0 +1,13 @@
+package com.walt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BicyleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
